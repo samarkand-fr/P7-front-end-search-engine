6671fe13ac5b3aa8f60c7657590854947cc69383
dina <jdina2004@gmail.com>
2023-03-23 10:16:37 +0100

    comment the code

--- index.js
+++ index.js
@@ -1,180 +1,37 @@
-
-
-
-
-// good with json file 
-// "use strict";
-
-// import { displayRecipeCards } from "./JS/factories/cards.js";
-// import { displayFilters } from "./JS/factories/filters.js";
-// import { isFiltersInteractive } from "./JS/interactiveFilters.js";
-// import { taggedItem, findRecipe } from "./JS/search.js";
-
-// // GET DATA
-// export const getRecipes = (async () => {
-//     await fetch("./recipes.json")
-//       .then((response) => {
-//         return response.json();
-//       })
-//       .then((data) => {
-//         displayRecipeList(data.recipes);
-  
-//       })
-//       .catch((error) => {
-//         error.message;
-//       });
-//   })();
-
-// // Constructor function for rendering recipes
-// export function RecipeRenderer(data) {
-//   this.data = data;
-//   this.returnRecipes = function (data) {
-//     console.log(data);
-//     return data;
-//   };
-// }
-
-// RecipeRenderer.prototype.getAllRecipes = function (recipes) {
-//   return displayRecipeList(recipes);
-// };
-
-// // Hydrate the components with the data retrieved from the API
-// const displayRecipeList = (recipes) => {
-//   findRecipe(recipes);
-//   displayFilters(recipes);
-//   taggedItem(recipes);
-//   displayRecipeCards(recipes);
-//   return recipes;
-// };
-
-// // Add event listeners to filter buttons for opening and closing the filters
-// const buttons = document.querySelectorAll(".filter__select");
-// buttons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     const buttonValue = button.getAttribute("value");
-//     isFiltersInteractive(button, buttonValue);
-//   });
-// });
-
-
-// import { displayRecipeCards } from "./JS/factories/cards.js";
-// import { displayFilters } from "./JS/factories/filters.js";
-// import { isFiltersInteractive } from "./JS/interactiveFilters.js";
-// import { taggedItem, findRecipe } from "./JS/search.js";
-// import { recipes } from "./recipes.js"; // import the recipes array
-
-// // Hydrate the components with the data retrieved from the API
-// const displayRecipeList = (recipes) => {
-//   findRecipe(recipes);
-//   displayFilters(recipes);
-//   taggedItem(recipes);
-//   displayRecipeCards(recipes);
-//   return recipes;
-// };
-// // Constructor function for rendering recipes
-// export function RecipeRenderer() {
-//   this.recipes = recipes;
-
-//   this.displayRecipeList = () => {
-//     findRecipe(this.recipes);
-//     displayFilters(this.recipes);
-//     taggedItem(this.recipes);
-//     displayRecipeCards(this.recipes);
-//     return this.recipes;
-//   };
-
-//   this.getAllRecipes = () => {
-//     return this.displayRecipeList();
-//   };
-// }
-// const recipeRenderer = new RecipeRenderer();
-// recipeRenderer.getAllRecipes();
-
-
-
-// // Add event listeners to filter buttons for opening and closing the filters
-// const buttons = document.querySelectorAll(".filter__select");
-// buttons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     const buttonValue = button.getAttribute("value");
-//     isFiltersInteractive(button, buttonValue);
-//   });
-// });
-
-// // Display the recipe list using the recipes data from recipes.js
-// displayRecipeList(recipes);
-
-
-
-
-// import { displayRecipeCards } from "./JS/factories/cards.js";
-// import { displayFilters } from "./JS/factories/filters.js";
-// import { isFiltersInteractive } from "./JS/interactiveFilters.js";
-// import { taggedItem, findRecipe } from "./JS/search.js";
-// import { recipes } from "./recipes.js";
-
-// // Constructor function for rendering recipes
-// export function RecipeRenderer(recipes) {
-//   this.recipes = recipes;
-
-//   this.displayRecipeList = () => {
-//     findRecipe(this.recipes);
-//     displayFilters(this.recipes);
-//     taggedItem(this.recipes);
-//     displayRecipeCards(this.recipes);
-//     return this;
-//   };
-
-//   this.getAllRecipes = () => {
-//     return this.displayRecipeList();
-//   };
-// }
-
-// const recipeRenderer = new RecipeRenderer(recipes);
-// recipeRenderer
-//   .displayRecipeList()
-
-
-// // Add event listeners to filter buttons for opening and closing the filters
-// const buttons = document.querySelectorAll(".filter__select");
-// buttons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     const buttonValue = button.getAttribute("value");
-//     isFiltersInteractive(button, buttonValue);
-//   });
-// });
 // Import modules
-import { displayRecipeCards } from "./scripts/components/cards.js";
-import { displayFilters } from "./scripts/components/filters.js";
-import { isFiltersInteractive } from "./scripts/FiltersController.js";
-import {  findRecipe } from "./scripts/search.js";
-import { recipes } from "./recipes.js";
+import { displayRecipeCards } from "./scripts/components/cards.js"; // import function to display recipe cards
+import { displayFilters } from "./scripts/components/filters.js"; // import function to display filters
+import { isFiltersInteractive } from "./scripts/FiltersController.js"; // import function to handle filter interaction
+import { taggedItem, findRecipe } from "./scripts/search.js"; // import functions to handle searching
+import { recipes } from "./recipes.js"; // import the recipe data
 
 // Define a class for rendering recipes
 class RecipeRenderer {
     constructor(recipes) {
-        this.recipes = recipes;
+        this.recipes = recipes; // set the recipes property of the instance
     }
 
+    // Render the recipes
     renderRecipes() {
-        displayRecipeCards(this.recipes);
-        findRecipe(this.recipes);
-        displayFilters(this.recipes);
-        return this.recipes;
+        displayRecipeCards(this.recipes); // display the recipe cards
+        findRecipe(this.recipes); // add search functionality
+        displayFilters(this.recipes); // display the filters
+        taggedItem(this.recipes); // add filtering functionality
+        return this.recipes; // return the recipes
     }
 }
 
 // Create a new instance of the RecipeRenderer class
-const recipeRenderer = new RecipeRenderer(recipes);
+const recipeRenderer = new RecipeRenderer(recipes); // pass in the recipe data
 
 // Add event listeners to filter buttons for opening and closing the filters
-const filterButtons = document.querySelectorAll(".filter__select");
-filterButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const buttonValue = button.getAttribute("value");
-        isFiltersInteractive(button, buttonValue);
+const filterButtons = document.querySelectorAll(".filter__select"); // select all filter buttons
+filterButtons.forEach((button) => { // loop through each button
+    button.addEventListener("click", () => { // add a click event listener to the button
+        const buttonValue = button.getAttribute("value"); // get the button's value attribute
+        isFiltersInteractive(button, buttonValue); // handle filter interaction
     });
 });
 
 // Render the recipe list
-recipeRenderer.renderRecipes();
+recipeRenderer.renderRecipes(); // call the renderRecipes method on the instance
