cd1f5515e4feec6cc6e6b016b3e2613d8d02a5c6
dina <jdina2004@gmail.com>
2023-03-23 10:42:08 +0100

    rectify the comments

--- scripts/utils.js
+++ scripts/utils.js
@@ -3,9 +3,12 @@ export const capitalize = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
-// Helper function to check if a recipe matches the search filter
-// @arguments object recipe and a string to check 
-// return boolean
+/** 
+* Helper function to check if a recipe matches the search filter
+ @param {Object} recipe - the recipe object to be checked
+ @param {string} filterLowerCase - the search filter string in lowercase
+ @returns {boolean} - true if the recipe matches the search filter, false otherwise
+*/
 export const matchesFilter = (recipe, filterLowerCase) => {
     const { name, description, appliance, ustensils, ingredients } = recipe;
     const includesFilter = (string) => string.toLowerCase().includes(filterLowerCase);
@@ -19,8 +22,12 @@ export const matchesFilter = (recipe, filterLowerCase) => {
     );
 };
 
-// function returns a boolean value indicating whether any of the ingredients match the filterLowerCase string.
-// takes in the ingredients array and the includesFilter function as arguments
+/** 
+* Function that returns a boolean value indicating whether any of the ingredients match the filterLowerCase string.
+ @param {Array} ingredients - an array of ingredient objects to be checked
+ @param {function} includesFilter - a function that takes a string argument and returns true if the string includes the search filter
+ @returns {boolean} - true if any of the ingredients match the search filter, false otherwise
+*/
 const ingredientsMatchFilter = (ingredients, includesFilter) => {
     return (
         ingredients.length > 0 &&
@@ -28,13 +35,15 @@ const ingredientsMatchFilter = (ingredients, includesFilter) => {
     );
 };
 
-// removes duplicate items from an array based on the id property of each item.
+/**
+ * @param {Array} array - The input array to remove duplicate items from.
+ * @returns {Array} - An array with unique items based on the `id` property of each item.
+ */
 export const removeDuplicateItems = (array) => {
-    let cleanArray = [];//store unique items
+    let cleanArray = []; // An array to store unique items.
     array.forEach((item) => {
-        // check  if there is already an item in cleanArray with the same id property as the current item
-        //returns +ve index i.e item already exist ,skip
-        // returns -1 no matching item ,add to cleanArray
+        // Check if there is already an item in `cleanArray` with the same `id` property as the current `item`.
+        // If the item already exists, skip it. Otherwise, add it to `cleanArray`.
         if (cleanArray.findIndex((el) => el.id === item.id) === -1) {
             cleanArray.push(item);
         }
