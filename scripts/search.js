a8da20c9f0b3e0dfbd3adcd21d135f7497823f69
dina <jdina2004@gmail.com>
2023-03-29 11:22:38 +0200

    load a native search

--- scripts/search.js
+++ scripts/search.js
@@ -1,44 +1,50 @@
-// Import modules
-import { displayRecipeCards } from "./components/cards.js";
-import { displayFilters } from "./components/filters.js";
-import { handleFilterReload } from "./FiltersController.js";
-import { matchesFilter } from "./utils.js";
+// Import necessary modules
+import { displayRecipeCards } from "./components/cards.js"; // function to display recipe cards
+import { displayFilters } from "./components/filters.js"; // function to display filter options
+import { handleFilterReload } from "./FiltersController.js"; // function to update filter status
+import { matchesFilter } from "./utils.js"; // utility functions checking for filter matches
 
+/**
+ * Searches through a list of recipes and returns the matching recipes with any duplicates removed.
+ * @param {array} recipes - The list of recipes to search through
+ * @param {string} filter - The search filter to apply to the recipes
+ * @returns {array} - The matching recipes with any duplicates removed
+*/
 export function getMatchingRecipes(recipes, filter) {
     // Convert the search filter to lowercase and remove any leading or trailing white spaces.
     const filterLowerCase = filter.toLowerCase().trim();
-    // Initialize an empty array to store the matching recipes.
-    const queriedCards = [];
+    // Initialize an empty set to store the matching recipes.
+    const queriedCards = new Set();
 
     // Loop through all the recipes and check if they match the search filter.
-    for (const recipe of recipes) {
+    for (let i = 0; i < recipes.length; i++) {
+        const recipe = recipes[i];
         if (matchesFilter(recipe, filterLowerCase)) {
-            queriedCards.push(recipe);
+            queriedCards.add(recipe);
         } else {
-            // If the recipe doesn't match the search filter,
             // check if its ustensils or ingredients contain the search filter.
-            for (const ustensil of recipe.ustensils) {
+            for (let j = 0; j < recipe.ustensils.length; j++) {
+                const ustensil = recipe.ustensils[j];
                 if (ustensil.includes(filterLowerCase)) {
-                    queriedCards.push(recipe);
-                    break;
+                    queriedCards.add(recipe);
                 }
             }
             // Check if any of the recipe's ingredients match the search filter.
-            for (const ingredient of recipe.ingredients) {
+            for (let k = 0; k < recipe.ingredients.length; k++) {
+                const ingredient = recipe.ingredients[k];
                 if (ingredient.ingredient.includes(filterLowerCase)) {
-                    queriedCards.push(recipe);
-                    break;
+                    queriedCards.add(recipe);
                 }
             }
         }
     }
-    // Use `new Set` to remove any duplicate items from the queried recipe cards and convert it back to an array.
-    return Array.from(new Set(queriedCards));
+    // Convert the Set to an array and return the result.
+    return [...queriedCards];
 }
 
 /**
- * Define a function to handle recipe searching
- * @param {array} recipes - array of objects
+* Define a function to handle recipe searching
+* @param {array} recipes - array of objects
 */
 export function findRecipe(recipes) {
     // Get the input element for the search box
@@ -59,23 +65,29 @@ export function findRecipe(recipes) {
             }, 300);
         } else {
             // If the search query is too short or empty, display all recipes and reset the filter status
-            displayRecipeAndFilter(recipes);
+            displayRecipeAndFilter(recipes, null);
         }
     });
 }
-
 /**
  * Display the recipe cards and filter options
  * @param {array} recipes - array of objects
  */
-function displayRecipeAndFilter(recipes) {
+function displayRecipeAndFilter(recipes, recipe) {
     // Display the search results on the page
     displayRecipeCards(recipes);
-    // Generate filter options based on the search results
-    displayFilters(recipes);
+    // Generate filter options based on the input recipe and the search results
+    displayFilters(recipes, recipe);
     // Update the filter status to reflect any changes in the search results
     handleFilterReload(recipes);
 }
+
+
+
+
+
+
+
 
 
 
